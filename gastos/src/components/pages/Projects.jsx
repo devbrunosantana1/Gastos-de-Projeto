0b80e7bf9beba3c94d74f9bad84f84ff2f32895a
Bruno Santana <104157394+devbrunosantana1@users.noreply.github.com>
2022-12-13 18:47:20 -0300

    create message success

--- gastos/src/components/pages/Projects.jsx
+++ gastos/src/components/pages/Projects.jsx
@@ -1,8 +1,19 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { Message } from "../layout/Message";
+import { useLocation } from "react-router-dom";
+
 export function Projects() {
+
+    const location = useLocation()
+    let message = ''
+    if(location.state) {
+        message = location.state.message
+    }
     return(
         <div>
-            <h1>Projects</h1>
+            <h1>Meus Projetos</h1>
+            {message && <Message type="success" msg={message}/>}
+        
             <Link to="/">retornar a página inicial</Link>
         </div>
     )
